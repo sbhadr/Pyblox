c4569f8e5f80cd915f8ce552cb28a67331570e64
Sanjay <Sanjay-B@users.noreply.github.com>
2017-11-08 20:50:56 -0500

    [Init] Exposed 'Groups' class
    Please check recent commits for more information.
    
    - Got rid of importing useless logging module

--- pyblox/__init__.py
+++ pyblox/__init__.py
@@ -13,10 +13,8 @@ __copyright__ = 'Copyright 2017 Sanjay-B(Sanjay Bhadra)'
 __version__ = '0.0.2'
 
 #Parent Class Modules
-from .api.assets import Assets #[]
-from .api.http import Http #[X]
-from .api.friends import Friends #[X]
-from .api.user import User #[X]
-
-#Other Modules
-import logging
+from .api.assets import Assets 
+from .api.http import Http 
+from .api.friends import Friends 
+from .api.user import User
+from .api.groups import Groups
